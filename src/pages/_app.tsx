754f680f79b473214def805c115c8b2342c9b63d
KIMHYUNJE <ranflir.sc@gmail.com>
2026-03-30 11:28:50 +0900

    feat:네비게이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,30 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push('/test');
+    // replace 뒤로가기를 방지하면서 이동가능
+    // back 뒤로가기
+  };
+
   return (
     <>
-      <header>Global Header</header>
+      <header>
+        {/* link 태그는 기본적으로 a태그와 사용법이 같다 */}
+        <Link href={'/'}>Index</Link>
+        &nbsp;
+        <Link href={'/search'}>Search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>Book/1</Link>
+        <div>
+          <button onClick={onClickButton}> /test page로 이동</button>
+        </div>
+      </header>
+
       <Component {...pageProps} />
     </>
   );
